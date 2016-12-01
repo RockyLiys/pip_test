7341e49010d99ccf780ba0b852d8efaab73fcc4d
rocky <RockyLiys@users.noreply.github.com>
2016-12-01 19:44:17 +0800

    Update setup.py
    uodate

--- setup.py
+++ setup.py
@@ -24,29 +24,6 @@ if "install" in sys.argv:
             break
 
 # version = __import__(PACKAGE_NAME).get_version()
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
 
 setup(
     name='rocky',
