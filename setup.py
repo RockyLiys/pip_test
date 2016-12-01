8d69d156d5407c1cc0be64a5c05a6bf556f15bbc
rocky <RockyLiys@users.noreply.github.com>
2016-12-01 19:10:16 +0800

    Update setup.py
    update

--- setup.py
+++ setup.py
@@ -24,6 +24,29 @@ if "install" in sys.argv:
             break
 
 # version = __import__(PACKAGE_NAME).get_version()
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
 
 setup(
     name='rocky',
@@ -34,6 +57,7 @@ setup(
     description=('A high-level Python core  that encourages '),
     license='',
     include_package_data=True,
+    zip_safe=False,
     packages=get_packages('rocky'),
     package_data=get_package_data('rocky'),
 
